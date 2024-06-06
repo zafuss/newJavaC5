d075d18dbc5a5e2d3ea8d252a5e6c69f1e0b47e2
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2024-06-06 09:44:42 +0700

    security_2

--- src/main/java/com/hutech/tests3/Services/CustomUserDetailService.java
+++ src/main/java/com/hutech/tests3/Services/CustomUserDetailService.java
@@ -1,18 +1,12 @@
 package com.hutech.tests3.Services;
 
+import com.hutech.tests3.Entities.CustomUserDetail;
 import com.hutech.tests3.Entities.User;
 import com.hutech.tests3.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Service
 public class CustomUserDetailService implements UserDetailsService {
@@ -23,22 +17,13 @@ public class CustomUserDetailService implements UserDetailsService {
     private UserService userService;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public CustomUserDetail loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
         if (user == null) {
             throw new UsernameNotFoundException(username);
         }
-        List<GrantedAuthority> listAuth = new ArrayList<>();
-        for (String role: BuildRolesFromRole(user.getRole().getRole_name())){
-            listAuth.add(new SimpleGrantedAuthority(role));
-        }
-        UserDetails userDetails =  org.springframework.security.core.userdetails.User.builder().username(user.getUsername()).password(user.getPassword()).authorities(listAuth).build();
-        return userDetails;
+        return new CustomUserDetail(user);
     }
-    public List<String> BuildRolesFromRole(String role){
-        String roles = "ADMIN,MODIFIER,USER";
-        int index = role.indexOf(role);
-        return Arrays.stream(roles.substring(index).split(",")).toList();
-    }
+
 
 }
